a9e38caba6615902ce1d45b9742d128edddcd34f
vidduwsc <viduc18701@gmail.com>
2022-07-02 16:37:30 +0700

    deploy to firebase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import AddTodo from "./components/AddTodo/AddTodo";
 import TodoList from "./components/TodoList/TodoList";
 import Filter from "./components/Filter/Filter";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD5JBb_QfFhKqC8dcAIoYPmQfFFtggQyOQ",
+  authDomain: "todo-list-1807vd.firebaseapp.com",
+  projectId: "todo-list-1807vd",
+  storageBucket: "todo-list-1807vd.appspot.com",
+  messagingSenderId: "343844414502",
+  appId: "1:343844414502:web:4fa0308b9d69dfe5adb2e7",
+  measurementId: "G-LRLJVPPMJ8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <div className="App">
